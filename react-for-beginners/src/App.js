2d89aa59cb77cf9c3469605d52b81ad53ee23346
Joungwoo Lee <ljw8124@gmail.com>
2023-02-02 21:22:47 +0900

    useEffect 활용

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,12 +1,16 @@
 import Button from './Button';
 import styles from './App.module.css';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 function App() {
   // 현재 코드는 state 가 변경될 때마다 모두 render 되고 있다.
   const [counter, setValue] = useState(0);
   const onClick = () => setValue(prev => prev + 1);
   console.log('render');
+  // useEffect 는 코드가 딱 한번만 실행될 수 있도록 보호해준다.
+  useEffect(() => {
+    console.log('I run only once');
+  }, []);
   return (
     <div>
       <h1 className={styles.title} onClick={onClick}>Welcome back! {counter}</h1>
