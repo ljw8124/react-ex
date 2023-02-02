09c6e8cb3280adfd3a6eef0ea5474e1984257d6d
Joungwoo Lee <ljw8124@gmail.com>
2023-02-02 21:23:08 +0900

    useEffect 가 두번 실행되는 이유

--- react-for-beginners/src/index.js
+++ react-for-beginners/src/index.js
@@ -4,6 +4,8 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    // React.StrictMode 가 있으면, 개발에서 오류를 잘잡기 위해서
+    // useEffect 도 두 번 찍힌다.
   <React.StrictMode>
     <App />
   </React.StrictMode>
