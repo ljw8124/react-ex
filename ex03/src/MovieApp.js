1e438e6a745b68ddd1d1d80c97768f3b144feefb
이정우 <ljw8124@gmail.com>
2023-02-13 17:17:55 +0900

    router 사용

--- ex03/src/MovieApp.js
+++ ex03/src/MovieApp.js
@@ -1,5 +1,20 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
+import Home from './routes/Home';
+import Detail from './routes/Detail';
+
 function MovieApp() {
-  return null;
+  return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie/:id" element={<Detail />}/>
+        </Routes>
+      </Router>
+  );
 }
 
 export default MovieApp;
