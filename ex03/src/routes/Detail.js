7ba41a5185893cb65288069ecf459c2024296088
Joungwoo Lee <ljw8124@gmail.com>
2023-02-14 16:01:12 +0900

    react detail 추거

--- ex03/src/routes/Detail.js
+++ ex03/src/routes/Detail.js
@@ -1,18 +1,43 @@
-import { useEffect } from 'react';
+import {useEffect, useState} from 'react';
 import { useParams } from 'react-router-dom';
 
 function Detail() {
   const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [details, setDetails] = useState([]);
   const getMovie = async () => {
     const json = await(
         await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+
+    setDetails(json.data.movie);
+    setLoading(false);
   }
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>
+
+  return (
+      <div>
+        { loading ? (
+            <h1>Loading....</h1>
+        ) : (
+            <div>
+              <img src={details.medium_cover_image} />
+              <h1>{details.title_long} ({details.rating})</h1>
+              <ul>
+                {details.genres.map(genre => <li key={genre}>{genre}</li>)}
+              </ul>
+              <p>
+                {details.description_full}
+              </p>
+              <h4>{details.date_uploaded}</h4>
+            </div>
+
+        )}
+      </div>
+
+  )
 }
 
 export default Detail;
