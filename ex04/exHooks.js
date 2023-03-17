6a66d04e91b3b652380141033effe3e621d812ca
이정우 <ljw8124@gmail.com>
2023-03-17 10:11:16 +0900

    useInput

--- ex04/exHooks.js
+++ ex04/exHooks.js
@@ -1,3 +1,8 @@
 // Code sandbox 를 활용하여, react App 을 생성하기 전에 실시간으로 변화하는 테스트가 가능함.
 // 온라인이기 때문에 로그인만 하면 바로 가능!
 // https://codesandbox.io/ 참고
+
+// hooks 은 react 의 state machine 에 연결하는 기본적인 방법이다. -> 모든것이 함수형 프로그래밍으로 변환가능해짐
+
+// 1. useState 에 관하여...
+//    useState 는 초기상태와 변한상태, 그리고 초기값을 셋팅할 수 있는 옵션들을 제공한다.
